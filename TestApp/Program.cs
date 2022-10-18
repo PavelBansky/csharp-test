705b15cc51d6f76297ce3bfc9000e485f21f5998
Pavel Bansky <pavel@bansky.net>
2022-10-18 12:24:23 +0200

    initial commit

---
+++ TestApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using System.Text;
+
+Console.WriteLine("Enter phrase:");
+MD5 md5 = MD5.Create();
+string s = Console.ReadLine();
+byte[] hash = md5.ComputeHash(Encoding.ASCII.GetBytes(s));
+
+foreach(byte b in hash)
+{
+    Console.Write(b.ToString("X2"));
+}
