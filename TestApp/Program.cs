bae9bec96d2212870c35281f8ed262d87f98b209
Pavel Bansky <pavel@bansky.net>
2024-04-28 08:24:16 -0700

    Update Program.cs

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,13 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Security.Cryptography;
 using System.Text;
 
-Console.WriteLine("Enter some value to be hashed:");
-MD5 md5 = MD5.Create();
-string s = Console.ReadLine();
-byte[] hash = md5.ComputeHash(Encoding.ASCII.GetBytes(s));
-
-foreach(byte b in hash)
+internal class Program
 {
-    Console.Write(b.ToString("X2"));
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter some value to be hashed:");
+        MD5 md5 = MD5.Create();
+        string s = Console.ReadLine();
+        byte[] hash = md5.ComputeHash(Encoding.ASCII.GetBytes(s));
+
+        foreach (byte b in hash)
+        {
+            Console.Write(b.ToString("X2"));
+        }
+    }
+
+    public SerializationBinder SomeBinder { get; set; }
+
+    public object DoDeserialization(byte[] bytes)
+    {
+        //BAD
+        var formatter = new BinaryFormatter();
+        formatter.Binder = SomeBinder;
+        return formatter.Deserialize(new MemoryStream(bytes));
+    }
+
+}
+
+[Serializable()]
+public class ExampleClass : IDeserializationCallback
+{
+    private string member;
+    void IDeserializationCallback.OnDeserialization(Object sender)
+    {
+        var sourceFileName = "malicious file";
+        var destFileName = "sensitive file";
+        File.Copy(sourceFileName, destFileName);
+    }
 }
