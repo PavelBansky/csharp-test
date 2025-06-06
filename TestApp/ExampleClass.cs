20c71b04b71779b6186e3d0734534fe8c0ba6291
Pavel Bansky <pavel@bansky.net>
2025-06-05 17:09:02 -0700

    some update

---
+++ TestApp/ExampleClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    [Serializable()]
+    public class ExampleClass : IDeserializationCallback
+    {
+        private string member;
+        void IDeserializationCallback.OnDeserialization(Object sender)
+        {
+            var sourceFileName = "malicious file";
+            var destFileName = "sensitive file";
+            File.Copy(sourceFileName, destFileName);
+        }
+
+        public SerializationBinder SomeBinder { get; set; }
+
+        public object DoDeserialization(byte[] bytes)
+        {
+            //BAD
+            var formatter = new BinaryFormatter();
+            formatter.Binder = SomeBinder;
+            return formatter.Deserialize(new MemoryStream(bytes));
+        }
+    }
+
+
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -9,9 +9,13 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Enter some value to be hashed:");
-        MD5 md5 = MD5.Create();
+
+
+
+        SHA1 sha1 = SHA1.Create();
+
         string s = Console.ReadLine();
-        byte[] hash = md5.ComputeHash(Encoding.ASCII.GetBytes(s));
+        byte[] hash = sha1.ComputeHash(Encoding.ASCII.GetBytes(s));
 
         foreach (byte b in hash)
         {
@@ -19,26 +23,6 @@ internal class Program
         }
     }
 
-    public SerializationBinder SomeBinder { get; set; }
-
-    public object DoDeserialization(byte[] bytes)
-    {
-        //BAD
-        var formatter = new BinaryFormatter();
-        formatter.Binder = SomeBinder;
-        return formatter.Deserialize(new MemoryStream(bytes));
-    }
 
 }
 
-[Serializable()]
-public class ExampleClass : IDeserializationCallback
-{
-    private string member;
-    void IDeserializationCallback.OnDeserialization(Object sender)
-    {
-        var sourceFileName = "malicious file";
-        var destFileName = "sensitive file";
-        File.Copy(sourceFileName, destFileName);
-    }
-}
